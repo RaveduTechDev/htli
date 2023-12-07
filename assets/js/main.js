d11ffaf8167ade2127fd27b3da410416adc24048
Ariel Wardana <112913828+ArielGwd@users.noreply.github.com>
2023-12-07 22:17:07 +0700

    update 8
    nambah pengantar

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,19 @@ navHumbergur.onclick = () => {
     navSlide.classList.toggle("slide")
 } 
 
+const textMom = document.querySelector(".pengantar-desc")
+
+textMom.onclick = (event) => {
+    const current = event.target
+
+    const btnSelengkapnya = current.className.includes("read-more-btn")
+    if (! btnSelengkapnya) return;
+
+    const currentText = event.target.parentNode.querySelector(".read-more-text")
+
+    currentText.classList.toggle('read-more-text--show')
+
+    current.textContent = current.textContent.includes('Selengkapnya') ? 
+    "Sembunyikan..." : "Selengkapnya..."
+    
+}
